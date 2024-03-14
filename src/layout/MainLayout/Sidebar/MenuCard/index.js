9d876653a277e5febf05865d8d7abf3f99921384
Danny <223181@ids.upchiapas.edu.mx>
2024-03-13 21:52:34 -0600

    Frond-end

---
+++ src/layout/MainLayout/Sidebar/MenuCard/index.js
@@ -0,0 +1,132 @@
+import PropTypes from 'prop-types';
+
+// material-ui
+import { styled, useTheme } from '@mui/material/styles';
+import {
+  Avatar,
+  Card,
+  CardContent,
+  Grid,
+  LinearProgress,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+  linearProgressClasses
+} from '@mui/material';
+
+
+
+// styles
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: 30,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: '#fff'
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: theme.palette.primary.main
+  }
+}));
+
+const CardStyle = styled(Card)(({ theme }) => ({
+  background: theme.palette.primary.light,
+  marginBottom: '22px',
+  overflow: 'hidden',
+  position: 'relative',
+  '&:after': {
+    content: '""',
+    position: 'absolute',
+    width: '157px',
+    height: '157px',
+    background: theme.palette.primary[200],
+    borderRadius: '50%',
+    top: '-105px',
+    right: '-96px'
+  }
+}));
+
+// ==============================|| PROGRESS BAR WITH LABEL ||============================== //
+
+function LinearProgressWithLabel({ value, ...others }) {
+  const theme = useTheme();
+
+  return (
+    <Grid container direction="column" spacing={1} sx={{ mt: 1.5 }}>
+      <Grid item>
+        <Grid container justifyContent="space-between">
+          <Grid item>
+            <Typography variant="h6" sx={{ color: theme.palette.primary[800] }}>
+              Progress
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h6" color="inherit">{`${Math.round(value)}%`}</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <BorderLinearProgress variant="determinate" value={value} {...others} />
+      </Grid>
+    </Grid>
+  );
+}
+
+LinearProgressWithLabel.propTypes = {
+  value: PropTypes.number
+};
+
+// ==============================|| SIDEBAR MENU Card ||============================== //
+
+const MenuCard = () => {
+  const theme = useTheme();
+
+  return (
+    <CardStyle>
+      <CardContent sx={{ p: 2 }}>
+        <List sx={{ p: 0, m: 0 }}>
+          <ListItem alignItems="flex-start" disableGutters sx={{ p: 0 }}>
+            <ListItemAvatar sx={{ mt: 0 }}>
+              <Avatar
+                variant="rounded"
+                sx={{
+                  ...theme.typography.commonAvatar,
+                  ...theme.typography.largeAvatar,
+                  color: theme.palette.primary.main,
+                  border: 'none',
+                  borderColor: theme.palette.primary.main,
+                  background: '#fff',
+                  marginRight: '12px'
+                }}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-router" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#00abfb" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M3 13m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
+                  <path d="M17 17l0 .01" />
+                  <path d="M13 17l0 .01" />
+                  <path d="M15 13l0 -2" />
+                  <path d="M11.75 8.75a4 4 0 0 1 6.5 0" />
+                  <path d="M8.5 6.5a8 8 0 0 1 13 0" />
+                </svg>
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              sx={{ mt: 0 }}
+              primary={
+                <Typography variant="subtitle1" sx={{ color: theme.palette.primary[800] }}>
+                  Conectado<br />Desconectado
+                </Typography>
+              }
+            // secondary={<Typography variant="caption"> 28/23 GB</Typography>}
+            />
+          </ListItem>
+        </List>
+        <LinearProgressWithLabel value={100} />
+      </CardContent>
+    </CardStyle>
+  );
+};
+
+export default MenuCard;
---
+++ src/ui-component/Logo.js
@@ -0,0 +1,28 @@
+// material-ui
+import logo from "../assets/images/icons/logo.png";
+/**
+ * if you want to use image instead of <svg> uncomment following.
+ *
+ * import logoDark from 'assets/images/logo-dark.svg';
+ * import logo from 'assets/images/logo.svg';
+ *
+ */
+
+// ==============================|| LOGO SVG ||============================== //
+
+const Logo = () => {
+  
+
+  return (
+
+    <img src={logo} alt="Berry" width="200" height="100" />
+    /**
+     * if you want to use image instead of svg uncomment following, and comment out <svg> element.
+     *
+     * <img src={logo} alt="Berry" width="100" />
+     *
+     */
+  );
+};
+
+export default Logo;

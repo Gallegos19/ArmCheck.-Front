e8e0aa81c9acb2449b3f1d7e8b62090af8fd7682
Gallegos19 <223204@ids.upchiapas.edu.mx>
2024-03-18 20:26:11 -0600

    Utilies off

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -1,12 +1,12 @@
 import dashboard from './dashboard';
 // import pages from './pages';
- import utilities from './utilities';
+// import utilities from './utilities';
 import others from './others';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard,utilities, others]
+  items: [dashboard, others]
 };
 
 export default menuItems;
